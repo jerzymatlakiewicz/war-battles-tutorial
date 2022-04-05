c775aa54365cbc0782972a1607917c4fa02c0880
Jerzy Matlakiewicz <jerzymatlakiewicz@gmail.com>
2022-04-05 22:46:05 +0200

    my first functionality added (hurray!) - now rocket explosions destroy tanks as well

--- main/rocket.go
+++ main/rocket.go
@@ -34,7 +34,7 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "collisionobject"
+  id: "hitco"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
   "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
@@ -63,6 +63,52 @@ embedded_components {
   "  data: 7.4535\n"
   "  data: 2.839\n"
   "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "explodeco"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"rockets\"\n"
+  "mask: \"tanks\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 20.4495\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
